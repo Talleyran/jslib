f166d314f309b7642c1e77a51af61752de84c4fb
Kirill Jakovlev <special-k@li.ru>
2012-02-03 18:21:27 +0400

    Adding tamplates and rake tasks class.

--- spec/jslib_spec.rb
+++ spec/jslib_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Jslib" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should create dir (root of library)" do
+    puts File.join File.expand_path(File.dirname(__FILE__)), '..', 'bin', 'jslib'
   end
 end
